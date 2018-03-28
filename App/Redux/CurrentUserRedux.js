9ae32dadb4f99e43375b712c90abfced4651d97e
Victor Lin <vlin@nextjump.com>
2018-03-28 12:52:40 -0400

    Complete current user redux

--- App/Redux/CurrentUserRedux.js
+++ App/Redux/CurrentUserRedux.js
@@ -6,7 +6,13 @@ import Immutable from 'seamless-immutable'
 const { Types, Creators } = createActions({
   setPseudonymRequest: ['pseudonym'],
   setPseudonymSuccess: ['pseudonym'],
-  setPseudonymFailure: null
+  setPseudonymFailure: null,
+  getUsersFollowingRequest: null,
+  getUsersFollowingSuccess: ['payload'],
+  getUsersFollowingFailure: null,
+  toggleUsersFollowingRequest: ['user'],
+  toggleUsersFollowingSuccess: ['payload'],
+  toggleUsersFollowingFailure: null,
 })
 
 export const CurrentUserTypes = Types
@@ -17,6 +23,7 @@ export default Creators
 export const INITIAL_STATE = Immutable({
   pseudonym: "Fixture User",
   fetching: null,
+  usersFollowing: null,
   error: null
 })
 
@@ -26,6 +33,8 @@ export const CurrentUserSelectors = {
 }
 
 /* ------------- Reducers ------------- */
+
+// Methods for setting your pseudonym
 
 // request the data from an api
 export const setPseudonymRequest = (state, { pseudonym }) =>
@@ -41,10 +50,48 @@ export const setPseudonymSuccess = (state, action) => {
 export const setPseudonymFailure = state =>
   state.merge({ fetching: false, error: true, pseudonym: null })
 
+// Methods for getting information about who you are and aren't following
+
+// request the data from an api
+export const getUsersFollowingRequest = (state, action) =>
+  state.merge({ fetching: true })
+
+// successful api lookup
+export const getUsersFollowingSuccess = (state, action) => {
+  const { payload } = action
+  return state.merge({ fetching: false, error: null, usersFollowing: payload })
+}
+
+// Something went wrong somewhere.
+export const getUsersFollowingFailure = state =>
+  state.merge({ fetching: false, error: true, usersFollowing: null })
+
+// Methods for setting information about who you are and aren't following
+
+// request the data from an api
+export const toggleUsersFollowingRequest = (state, action) =>
+  state.merge({ fetching: true })
+
+// successful api lookup
+export const toggleUsersFollowingSuccess = (state, action) => {
+  const { payload } = action
+  return state.merge({ fetching: false, error: null, usersFollowing: payload })
+}
+
+// Something went wrong somewhere.
+export const toggleUsersFollowingFailure = state =>
+  state.merge({ fetching: false, error: true, usersFollowing: null })
+
 /* ------------- Hookup Reducers To Types ------------- */
 
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.SET_PSEUDONYM_REQUEST]: setPseudonymRequest,
   [Types.SET_PSEUDONYM_SUCCESS]: setPseudonymSuccess,
-  [Types.SET_PSEUDONYM_FAILURE]: setPseudonymFailure
+  [Types.SET_PSEUDONYM_FAILURE]: setPseudonymFailure,
+  [Types.GET_USERS_FOLLOWING_REQUEST]: getUsersFollowingRequest,
+  [Types.GET_USERS_FOLLOWING_SUCCESS]: getUsersFollowingSuccess,
+  [Types.GET_USERS_FOLLOWING_FAILURE]: getUsersFollowingFailure,
+  [Types.TOGGLE_USERS_FOLLOWING_REQUEST]: toggleUsersFollowingRequest,
+  [Types.TOGGLE_USERS_FOLLOWING_SUCCESS]: toggleUsersFollowingSuccess,
+  [Types.TOGGLE_USERS_FOLLOWING_FAILURE]: toggleUsersFollowingFailure,
 })
