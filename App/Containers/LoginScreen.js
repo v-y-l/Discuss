3657cae78448f12a66533bb9ed9413daa6f2e63c
Victor Lin <vlin@nextjump.com>
2018-04-20 15:04:37 -0400

    Continue working on discuss app login screen

--- App/Containers/LoginScreen.js
+++ App/Containers/LoginScreen.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
-import { ScrollView, Text, KeyboardAvoidingView, Button } from 'react-native';
+import { View, Text, KeyboardAvoidingView, Button, TextInput } from 'react-native';
 import { connect } from 'react-redux';
-// Add Actions - replace 'Your' with whatever your reducer is called :)
-// import YourActions from '../Redux/YourRedux'
 
 // Styles
 import styles from './Styles/LoginScreenStyle';
 
 class LoginScreen extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+
   render() {
     return (
-      <ScrollView style={styles.container}>
+      <View style={styles.container}>
         <KeyboardAvoidingView behavior="position">
-          <Text>LoginScreen</Text>
-          <Button title="Login" onPress={()=>{this.props.navigation.navigate("AppScreen");}} />
+          <Text style={styles.header}>Discuss</Text>
+          <View style={styles.loginBox}>
+            <TextInput 
+              style={styles.loginItem} 
+              onChangeText={(username)=>this.setState({username})}
+              value={this.state.username}
+              placeholder="Enter your User ID"
+            />
+            <TextInput 
+              style={styles.loginItem}
+              onChangeText={(password)=>this.setState({password})}
+              value={this.state.password}
+              placeholder="Enter your Password" 
+            />
+            <Button style={styles.loginItem} title="Login" onPress={()=>{this.props.navigation.navigate("AppScreen");}} />
+          </View>
         </KeyboardAvoidingView>
-      </ScrollView>
+      </View>
     );
   }
 }
--- App/Containers/Styles/LoginScreenStyle.js
+++ App/Containers/Styles/LoginScreenStyle.js
@@ -2,5 +2,25 @@ import { StyleSheet } from 'react-native';
 import { ApplicationStyles } from '../../Themes/';
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgb(189,0,255)',
+  },
+  header: {
+    fontSize: 75,
+    color: 'white',
+  },
+  loginBox: {
+    margin: 8,
+    backgroundColor: 'white',
+    alignItems: 'center',
+  },
+  loginItem: {
+    marginBottom: 5,
+    borderBottomWidth: 3,
+    borderBottomColor: '#E0E0E0',
+  }
 });
+
