5a2128a6f4ef001d550a62da68d0c6b7c482e44a
Victor Lin <vlin@nextjump.com>
2018-04-20 15:11:07 -0400

    Continue working on login styles

--- App/Containers/Styles/LoginScreenStyle.js
+++ App/Containers/Styles/LoginScreenStyle.js
@@ -1,25 +1,28 @@
 import { StyleSheet } from 'react-native';
 import { ApplicationStyles } from '../../Themes/';
+import { Fonts } from '../../Themes/';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgb(189,0,255)',
+    backgroundColor: 'rgb(170,76,186)',
   },
   header: {
+    fontFamily: Fonts.type.base,
     fontSize: 75,
     color: 'white',
   },
   loginBox: {
     margin: 8,
     backgroundColor: 'white',
-    alignItems: 'center',
+    alignItems: 'stretch',
   },
   loginItem: {
-    marginBottom: 5,
-    borderBottomWidth: 3,
+    padding: 10,
+    margin: 8,
+    borderBottomWidth: 1,
     borderBottomColor: '#E0E0E0',
   }
 });
